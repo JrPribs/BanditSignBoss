8c9dff899fa13b16b0cd8712c21ad7c56fd8de48
John Pribesh <beopensourcellc@gmail.com>
2014-11-04 22:30:09 -0500

    coords display working

--- bsbApp/routes/uploads.js
+++ bsbApp/routes/uploads.js
@@ -11,61 +11,65 @@ router.get('/', function(req, res) {
 });
 
 router.post("/", function(req, res, next) {
-    if (req.files) {
-        console.log(util.inspect(req.files));
-        if (req.files.size === 0) {
-            return next(new Error("Why didn't you select a file?"));
-        }
-        fs.exists(req.files.myFile.path, function(exists) {
-            if (exists) {
-            	var geoCoords = '';
-            	var path = req.files.myFile.path;
-                im.readMetadata('./' + path, function(error, metadata) {
-                    if (error) throw error;
-                    var lat = getDegrees(metadata.exif.gpsLatitude.split(','));
-                    var latRef = metadata.exif.gpsLatitudeRef;
-                    if (latRef === 'S') {
-                        lat = lat * -1;
-                    }
-                    var lng = getDegrees(metadata.exif.gpsLongitude.split(','));
-                    var lngRef = metadata.exif.gpsLongitudeRef;
-                    if (lngRef === 'W') {
-                        lng = lng * -1;
-                    }
-                    var coordinate = {
-                        lat: lat,
-                        lng: lng
-                    };
-                    geoCoords = coordinate.lat + ' ' + coordinate.lng;
-                    console.log(geoCoords);
-                });
-
-                function getDegrees(lat) {
-                    var degrees = 0;
-                    for (var i = 0; i < lat.length; i++) {
-                        var cleanNum = lat[i].replace(' ', '');
-                        var parts = cleanNum.split('/');
-                        var coord = parseInt(parts[0]) / parseInt(parts[1]);
-                        if (i == 1) {
-                            coord = coord / 60;
-                        } else if (i == 2) {
-                            coord = coord / 3600;
-                        }
-                        degrees += coord;
-                    }
-                    return degrees;
+            if (req.files) {
+                console.log(util.inspect(req.files));
+                if (req.files.size === 0) {
+                    return next(new Error("Why didn't you select a file?"));
                 }
+                    fs.exists(req.files.myFile.path, function(exists) {
+                        if (exists) {
+                            var path = req.files.myFile.path;
 
-                var upFile = req.files.myFile.name;
-                res.render("uploadSuccessPage", {
-                    title: "File Uploaded Successfully!",
-                    file: upFile,
-                    coords: geoCoords
-                });
-            } else {
-                res.end("File upload failed! You suck!");
-            }
-        });
-    }
-});
-module.exports = router;
+                            function getGeoLoc(path, callback) {
+                                im.readMetadata('./' + path, function(error, metadata) {
+                                    if (error) throw error;
+                                    var lat = getDegrees(metadata.exif.gpsLatitude.split(','));
+                                    var latRef = metadata.exif.gpsLatitudeRef;
+                                    if (latRef === 'S') {
+                                        lat = lat * -1;
+                                    }
+                                    var lng = getDegrees(metadata.exif.gpsLongitude.split(','));
+                                    var lngRef = metadata.exif.gpsLongitudeRef;
+                                    if (lngRef === 'W') {
+                                        lng = lng * -1;
+                                    }
+                                    var coordinate = {
+                                        lat: lat,
+                                        lng: lng
+                                    };
+                                    geoCoords = coordinate.lat + ' ' + coordinate.lng;
+                                    callback(geoCoords);
+                                });
+                            }
+
+                            function getDegrees(lat) {
+                                var degrees = 0;
+                                for (var i = 0; i < lat.length; i++) {
+                                    var cleanNum = lat[i].replace(' ', '');
+                                    var parts = cleanNum.split('/');
+                                    var coord = parseInt(parts[0]) / parseInt(parts[1]);
+                                    if (i == 1) {
+                                        coord = coord / 60;
+                                    } else if (i == 2) {
+                                        coord = coord / 3600;
+                                    }
+                                    degrees += coord;
+                                }
+                                return degrees;
+                            }
+
+                            getGeoLoc(path, function(geoCoords) {
+                                var upFile = req.files.myFile.name;
+                                res.render("uploadSuccessPage", {
+                                    title: "File Uploaded Successfully!",
+                                    file: upFile,
+                                    coords: geoCoords
+                                });
+                            });
+
+                        } else {
+                            res.end("File upload failed! You suck!");
+                        }
+                    });
+                }
+            }); module.exports = router;
