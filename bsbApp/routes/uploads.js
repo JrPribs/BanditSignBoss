fc5f6a4c3b6bd614612a097b679782e9d146e433
John Pribesh <beopensourcellc@gmail.com>
2014-11-05 01:00:10 -0500

    adjusted code for multiple images. Tested working.

--- bsbApp/routes/uploads.js
+++ bsbApp/routes/uploads.js
@@ -11,65 +11,106 @@ router.get('/', function(req, res) {
 });
 
 router.post("/", function(req, res, next) {
-            if (req.files) {
-                console.log(util.inspect(req.files));
-                if (req.files.size === 0) {
-                    return next(new Error("Why didn't you select a file?"));
+    if (req.files) {
+        console.log(util.inspect(req.files));
+        if (req.files.size === 0) {
+            return next(new Error("Why didn't you select a file?"));
+        }
+
+
+        function gatherImages(files, callback) {
+            var uploads = [];
+            var count = 0;
+            files.forEach(function(file) {
+
+                fs.exists(file.path, function(exists) {
+                    if (exists) {
+                        var path = file.path;
+                        var upFile = file.name;
+                        uploads.push({
+                            file: upFile,
+                            imgPath: path
+                        });
+                        count++;
+                    }
+                    if (files.length === count) {
+                        callback(uploads);
+                    }
+                });
+
+            });
+
+        }
+
+        function getGeoLoc(path, callback) {
+            im.readMetadata('./' + path, function(error, metadata) {
+                if (error) throw error;
+                var lat = getDegrees(metadata.exif.gpsLatitude.split(','));
+                var latRef = metadata.exif.gpsLatitudeRef;
+                if (latRef === 'S') {
+                    lat = lat * -1;
                 }
-                    fs.exists(req.files.myFile.path, function(exists) {
-                        if (exists) {
-                            var path = req.files.myFile.path;
-
-                            function getGeoLoc(path, callback) {
-                                im.readMetadata('./' + path, function(error, metadata) {
-                                    if (error) throw error;
-                                    var lat = getDegrees(metadata.exif.gpsLatitude.split(','));
-                                    var latRef = metadata.exif.gpsLatitudeRef;
-                                    if (latRef === 'S') {
-                                        lat = lat * -1;
-                                    }
-                                    var lng = getDegrees(metadata.exif.gpsLongitude.split(','));
-                                    var lngRef = metadata.exif.gpsLongitudeRef;
-                                    if (lngRef === 'W') {
-                                        lng = lng * -1;
-                                    }
-                                    var coordinate = {
-                                        lat: lat,
-                                        lng: lng
-                                    };
-                                    geoCoords = coordinate.lat + ' ' + coordinate.lng;
-                                    callback(geoCoords);
-                                });
-                            }
-
-                            function getDegrees(lat) {
-                                var degrees = 0;
-                                for (var i = 0; i < lat.length; i++) {
-                                    var cleanNum = lat[i].replace(' ', '');
-                                    var parts = cleanNum.split('/');
-                                    var coord = parseInt(parts[0]) / parseInt(parts[1]);
-                                    if (i == 1) {
-                                        coord = coord / 60;
-                                    } else if (i == 2) {
-                                        coord = coord / 3600;
-                                    }
-                                    degrees += coord;
-                                }
-                                return degrees;
-                            }
-
-                            getGeoLoc(path, function(geoCoords) {
-                                var upFile = req.files.myFile.name;
-                                res.render("uploadSuccessPage", {
-                                    title: "File Uploaded Successfully!",
-                                    file: upFile,
-                                    coords: geoCoords
-                                });
-                            });
-
-                        } else {
-                            res.end("File upload failed! You suck!");
-                        }
-                    });
+                var lng = getDegrees(metadata.exif.gpsLongitude.split(','));
+                var lngRef = metadata.exif.gpsLongitudeRef;
+                if (lngRef === 'W') {
+                    lng = lng * -1;
                 }
-            }); module.exports = router;
+                var coordinate = {
+                    lat: lat,
+                    lng: lng
+                };
+                geoCoords = coordinate.lat + ' ' + coordinate.lng;
+                callback(geoCoords);
+            });
+        }
+
+        function getDegrees(lat) {
+            var degrees = 0;
+            for (var i = 0; i < lat.length; i++) {
+                var cleanNum = lat[i].replace(' ', '');
+                var parts = cleanNum.split('/');
+                var coord = parseInt(parts[0]) / parseInt(parts[1]);
+                if (i == 1) {
+                    coord = coord / 60;
+                } else if (i == 2) {
+                    coord = coord / 3600;
+                }
+                degrees += coord;
+            }
+            return degrees.toFixed(6);
+        }
+
+
+
+        function processImages(uploads, callback) {
+            var finalImages = [];
+            var count = 0;
+            uploads.forEach(function(upload) {
+            	var path = upload.imgPath;
+                getGeoLoc(path, function(geoCoords) {
+                    upload.coords = geoCoords;
+                    finalImages.push(upload);
+                    count++;
+                    if (uploads.length === count) {
+                    	callback(finalImages);
+                    }
+                });
+            });
+        }
+
+
+        gatherImages(req.files.myFile, function(uploads) {
+            processImages(uploads, function(finalImages) {
+                res.render("uploadSuccessPage", {
+                    title: "File(s) Uploaded Successfully!",
+                    files: finalImages
+                });
+            });
+        });
+
+
+
+
+    }
+});
+module.exports = router;
