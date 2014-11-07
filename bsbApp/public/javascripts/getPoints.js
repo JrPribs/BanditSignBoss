dad413d1ffabaeac0b9f50b116993b0910cb0d9b
John Pribesh <beopensourcellc@gmail.com>
2014-11-07 03:26:57 -0500

    cleaned up marker adding. verified working as intended. added pic highlight & info window on point click, and overlay on pic click

--- bsbApp/public/javascripts/getPoints.js
+++ bsbApp/public/javascripts/getPoints.js
@@ -13,27 +13,34 @@ $(document).ready(function() {
     }
 
     function initialize(points) {
-        var myLatlng = new google.maps.LatLng(36.771805, -76.244589);
         var mapOptions = {
-            center: myLatlng,
-            zoom: 14
+            zoom: 12
         };
+        var bounds = new google.maps.LatLngBounds();
         var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
+        var infowindow = new google.maps.InfoWindow();
         for (var i = 0; i < points.length; i++) {
             var image = 'images/' + points[i].file;
             var latlng = points[i].coords.split(' ');
             var marker = new google.maps.Marker({
-                position: {
-                    lat: parseFloat(latlng[0]),
-                    lng: parseFloat(latlng[1])
-                },
+                position: new google.maps.LatLng(latlng[0], latlng[1]),
                 map: map,
                 animation: google.maps.Animation.DROP,
                 title: points[i].file
             });
-            google.maps.event.addListener(marker, 'click', toggleBounce);
-        };
+            bounds.extend(marker.position);
+            
+            google.maps.event.addListener(marker, 'click', (function(marker, i){
+            	return function() {
+            		var current = marker.position.k + ' ' + marker.position.B.toFixed(6);
+            		$('.points').css('border', 'none');
+            		$('img[data-coords="' + current + '"]').css('border', '5px solid #F00');
+            		infowindow.setContent('<img src="/images/' + points[i].file + '" width="100px" height="100px">');
+            		infowindow.open(map, marker);
+            	}
+            })(marker, i));
+        }
+        map.fitBounds(bounds);
 
         function toggleBounce() {
             if (marker.getAnimation() != null) {
@@ -42,8 +49,26 @@ $(document).ready(function() {
                 marker.setAnimation(google.maps.Animation.BOUNCE);
             }
         }
+        var w = window,
+            d = document,
+            e = d.documentElement,
+            g = d.getElementsByTagName('body')[0],
+            x = w.innerWidth || e.clientWidth || g.clientWidth,
+            y = w.innerHeight || e.clientHeight || g.clientHeight;
+            h = y - 45;
+        $('#map-canvas').css('height', h + "px");
     }
     makePoints(function(points) {
         google.maps.event.addDomListener(window, 'load', initialize(points));
+    });
+    var $overlay = $('<img style="display:none;width:100%;height:100%;position:absolute;top:0;left:0;">');
+    $('body').append($overlay);
+    $('.points').click(function(){
+    	var url = $(this).attr('src');
+    	$overlay.attr('src', url);
+    	$overlay.show();
+    });
+    $overlay.click(function(){
+    	$overlay.hide();
     });
 });
